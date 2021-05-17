4261096e3e61daf3c61eafd54dc11c538e4e412a
Alice Bottino <s.trippstr@gmail.com>
2021-05-17 05:58:16 -0700

    t

--- Editor/BlaseSpry.cs
+++ Editor/BlaseSpry.cs
@@ -1,28 +1,21 @@
-﻿//#if UNITY_EDITOR
-using System;
+﻿#if UNITY_Editor
 using System.IO;
-using System.Text.RegularExpressions;
 using UnityEditor;
-
 using UnityEditor.Callbacks;
 
-public static class BlaseSpry
+namespace Arachnid
 {
-    [MenuItem("Arachnid/Set Template")]
-    public static void SetArachnidTemplate()
+    public static class BlaseSpry
     {
-        PlayerSettings.WebGL.template = "PROJECT:../../Packages/com.bottinogames.arachnid/WebGLTemplates/Arachnid";
+        //This get's rid of the warning screen Unity creates for mobile browsers. From: https://answers.unity.com/questions/1339261/unity-webgl-disable-mobile-warning.html
+        [PostProcessBuild]
+        public static void RemoveUnsupportedPopUp(BuildTarget target, string targetPath)
+        {
+            var path = Path.Combine(targetPath, "Build/UnityLoader.js");
+            var text = File.ReadAllText(path);
+            text = text.Replace("UnityLoader.SystemInfo.mobile", "false");
+            File.WriteAllText(path, text);
+        }
     }
-
-    //This get's rid of the warning screen Unity creates for mobile browsers. From: https://answers.unity.com/questions/1339261/unity-webgl-disable-mobile-warning.html
-    //[PostProcessBuild]
-    //public static void RemoveUnsupportedPopUp(BuildTarget target, string targetPath)
-    //{
-    //    var path = Path.Combine(targetPath, "Build/UnityLoader.js");
-    //    var text = File.ReadAllText(path);
-    //    text = text.Replace("UnityLoader.SystemInfo.mobile", "false");
-    //    File.WriteAllText(path, text);
-    //}
-
 }
-//#endif
+#endif
---
+++ Editor/TemplateInstaller.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class TemplateInstaller
+{
+    [InitializeOnLoadMethod]
+    public static void TemplatePrompt()
+    {
+        if (!AssetDatabase.IsValidFolder("WebGLTemplates/Arachnid"))
+        {
+            FileUtil.CopyFileOrDirectory("../Packages/com.bottinogames.arachnid/WebGLTemplates/Arachnid", "WebGLTemplates/Arachnid");
+        }
+    }
+}
+#endif
