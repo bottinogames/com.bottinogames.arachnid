0e0c3af5d0680522ba3234818d3ada28057b8531
Alice Bottino <s.trippstr@gmail.com>
2021-05-17 05:34:34 -0700

    test

--- Editor/BlaseSpry.cs
+++ Editor/BlaseSpry.cs
@@ -15,27 +15,14 @@ public static class BlaseSpry
     }
 
     //This get's rid of the warning screen Unity creates for mobile browsers. From: https://answers.unity.com/questions/1339261/unity-webgl-disable-mobile-warning.html
-    [PostProcessBuild]
-    public static void RemoveUnsupportedPopUp(BuildTarget target, string targetPath)
-    {
-        var path = Path.Combine(targetPath, "Build/UnityLoader.js");
-        var text = File.ReadAllText(path);
-        text = text.Replace("UnityLoader.SystemInfo.mobile", "false");
-        File.WriteAllText(path, text);
-    }
+    //[PostProcessBuild]
+    //public static void RemoveUnsupportedPopUp(BuildTarget target, string targetPath)
+    //{
+    //    var path = Path.Combine(targetPath, "Build/UnityLoader.js");
+    //    var text = File.ReadAllText(path);
+    //    text = text.Replace("UnityLoader.SystemInfo.mobile", "false");
+    //    File.WriteAllText(path, text);
+    //}
 
-    //Theoretically should fix the sizing issue. partially from: https://stackoverflow.com/questions/58381516/unity-webgl-build-how-to-set-canvas-size-for-mobile-browsers
-    [PostProcessBuild]
-    public static void MakeResponsiveToBrowserSize (BuildTarget target, string targetPath)
-    {
-        var path = Path.Combine(targetPath, "index.html");
-        string[] lines = File.ReadAllLines(path);
-        for (int i = 0; i < lines.Length; i++)
-        {
-            if (lines[i].Contains("div id=\"unityContainer\""))
-                lines[i] = "    <div id=\"unityContainer\" style=\"width: 360px; height: 640px; margin: auto\"></div>";
-        }
-        File.WriteAllLines(path, lines);
-    }
 }
 //#endif
